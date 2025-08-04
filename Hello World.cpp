6e4866caf99f35adac64abb157656aba7c1142e4
Prathyush2007 <prathyush.nimmagadda.btech2024@sitpune.edu.in>
2025-08-05 01:21:42 +0530

    Create Hello World.cpp

---
+++ Hello World.cpp
@@ -0,0 +1,11 @@
+//Prathyush Nimmagadda
+//24070123064
+//ENTC A3
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello, World!" << endl;
+    return 0;
+}
+/*Output : Hello, World!
