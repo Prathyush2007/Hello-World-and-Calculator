4e97e0d90c221da9a41c33f57e557989036fc1e6
Prathyush2007 <prathyush.nimmagadda.btech2024@sitpune.edu.in>
2025-08-05 01:22:17 +0530

    Create Basic Calculator.cpp

---
+++ Basic Calculator.cpp
@@ -0,0 +1,27 @@
+//Prathyush Nimmagadda
+//24070123064
+//ENTC A3
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+
+    cout << "Enter two numbers: ";
+    cin >> a >> b;
+
+    cout << "Addition: " << a + b << endl;
+    cout << "Subtraction: " << a - b << endl;
+    cout << "Multiplication: " << a * b << endl;
+    cout << "Division: " << a / b << endl;
+
+    return 0;
+}
+/*Enter two numbers: 3
+4
+Output : Addition: 7
+Subtraction: -1
+Multiplication: 12
+Division: 0
+*/
+
